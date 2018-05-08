7811d1157807c74a4d3c0110316f66526d6f3b2f
Michael Everitt <m.j.everitt@iee.org>
2018-05-09 00:18:04 +0100

    Update arm CHOST triplets to remove soft/hard-float from vendor field

--- catalyst/arch/arm.py
+++ catalyst/arch/arm.py
@@ -37,70 +37,70 @@ class arch_armv4tl(generic_arm):
 	"Builder class for armv4tl target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv4tl-softfloat-linux-gnueabi"
+		self.settings["CHOST"]="armv4tl-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv4t"
 
 class arch_armv5tl(generic_arm):
 	"Builder class for armv5tl target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv5tl-softfloat-linux-gnueabi"
+		self.settings["CHOST"]="armv5tl-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv5t"
 
 class arch_armv5tel(generic_arm):
 	"Builder class for armv5tel target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv5tel-softfloat-linux-gnueabi"
+		self.settings["CHOST"]="armv5tel-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv5te"
 
 class arch_armv5tejl(generic_arm):
 	"Builder class for armv5tejl target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv5tejl-softfloat-linux-gnueabi"
+		self.settings["CHOST"]="armv5tejl-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv5te"
 
 class arch_armv6j(generic_arm):
 	"Builder class for armv6j target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6j-softfp-linux-gnueabi"
+		self.settings["CHOST"]="armv6j-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv6j -mfpu=vfp -mfloat-abi=softfp"
 
 class arch_armv6z(generic_arm):
 	"Builder class for armv6z target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6z-softfp-linux-gnueabi"
+		self.settings["CHOST"]="armv6z-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv6z -mfpu=vfp -mfloat-abi=softfp"
 
 class arch_armv6zk(generic_arm):
 	"Builder class for armv6zk target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6zk-softfp-linux-gnueabi"
+		self.settings["CHOST"]="armv6zk-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv6zk -mfpu=vfp -mfloat-abi=softfp"
 
 class arch_armv7a(generic_arm):
 	"Builder class for armv7a target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7a-softfp-linux-gnueabi"
+		self.settings["CHOST"]="armv7a-unknown-linux-gnueabi"
 		self.settings["COMMON_FLAGS"]+=" -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
 
 class arch_armv6j_hardfp(generic_arm):
 	"Builder class for armv6j hardfloat target, needs >=gcc-4.5"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6j-hardfloat-linux-gnueabi"
+		self.settings["CHOST"]="armv6j-unknown-linux-gnueabihf"
 		self.settings["COMMON_FLAGS"]+=" -march=armv6j -mfpu=vfp -mfloat-abi=hard"
 
 class arch_armv7a_hardfp(generic_arm):
 	"Builder class for armv7a hardfloat target, needs >=gcc-4.5"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7a-hardfloat-linux-gnueabi"
+		self.settings["CHOST"]="armv7a-unknown-linux-gnueabihf"
 		self.settings["COMMON_FLAGS"]+=" -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard"
 
 class arch_armv5teb(generic_armeb):
@@ -108,7 +108,7 @@ class arch_armv5teb(generic_armeb):
 	def __init__(self,myspec):
 		generic_armeb.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]+=" -mcpu=xscale"
-		self.settings["CHOST"]="armv5teb-softfloat-linux-gnueabi"
+		self.settings["CHOST"]="armv5teb-unknown-linux-gnueabi"
 
 def register():
 	"Inform main catalyst program of the contents of this plugin."
